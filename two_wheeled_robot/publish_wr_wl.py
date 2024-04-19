32da55750cc61b1369403fcd2ad57bf9f4b38536
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2024-04-19 16:46:36 -0600

    Changed the publishwr_wl node to publish wheel speeds from robot vel instead of using the wheel pose, it is now less realistic but way more lighter

--- two_wheeled_robot/publish_wr_wl.py
+++ two_wheeled_robot/publish_wr_wl.py
@@ -1,91 +1,48 @@
 #! /usr/bin/env python3
-import numpy as np 
 from std_msgs.msg import Float32
+from geometry_msgs.msg import Twist
 import rclpy
 from rclpy.node import Node
-from tf2_ros import TransformException
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
-from tf_transformations import euler_from_quaternion, quaternion_from_euler
+
 
 
 class PublishWheelSpeeds(Node):
 
   def __init__(self):
     super().__init__('publish_wr_wl')
-
-    self.tf_buffer = Buffer()
-    self.tf_listener = TransformListener(self.tf_buffer, self)
-
-
+    self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_cb, 10 )
     # Create wr publisher
-    self.pub_wr = self.create_publisher(Float32, 'wr', 10)
+    self.pub_wr = self.create_publisher(Float32, 'VelocityEncR', 10)
     # Create wr publisher
-    self.pub_wl = self.create_publisher(Float32, 'wl', 10)
+    self.pub_wl = self.create_publisher(Float32, 'VelocityEncL', 10)
 
-    #
-    self.prev_theta_r = 0.0 #previous value of theta r
-    self.current_theta_r = 0.0 #current value of theta r
-    self.prev_theta_l = 0.0
-    self.current_theta_l = 0.0
-
+    #Robot parameters
+    self.L = 0.52 # wheel separation [m]
+    self.r = 0.14 # radius of the wheels in [m]
     self.wr = Float32()
     self.wl = Float32()
+    self.v = 0.0
+    self.w = 0.0
+    self.robot_vel = Twist()
 
     # Call on_timer function every second
-    self.dt= 0.1
+    self.dt= 0.02
     self.timer = self.create_timer(self.dt, self.on_timer)
 
   def on_timer(self):
-
-    # Look up for the transformation between target_frame and turtle2 frames
-    # and send velocity commands for turtle2 to reach target_frame
-    try:
-      tf_right = self.tf_buffer.lookup_transform(
-      "drivewhl_r_link",
-      "base_link",
-      rclpy.time.Time())
-    except TransformException as ex:
-      self.get_logger().info('Could not make the transformation from right_wheel to base_link transform')
-      return
-    try:
-      tf_left = self.tf_buffer.lookup_transform(
-      "drivewhl_l_link",
-      "base_link",
-      rclpy.time.Time())
-    except TransformException as ex:
-      self.get_logger().info('Could not make the transformation from left_wheel to base_link transform')
-      return
-
-
-    qx=tf_right.transform.rotation.x
-    qy=tf_right.transform.rotation.y
-    qz=tf_right.transform.rotation.z
-    qw=tf_right.transform.rotation.w
-    [rx,self.current_theta_r,rz]=euler_from_quaternion([qx,qy,qz,qw])
-
-    self.wr.data=(self.current_theta_r-self.prev_theta_r)/self.dt
-    self.prev_theta_r = self.current_theta_r
-
-    qx=tf_left.transform.rotation.x
-    qy=tf_left.transform.rotation.y
-    qz=tf_left.transform.rotation.z
-    qw=tf_left.transform.rotation.w
-
-
-    [rx,self.current_theta_l,rz]=euler_from_quaternion([qx,qy,qz,qw])
-
-    self.wl.data=(self.current_theta_l-self.prev_theta_l)/self.dt
-    self.prev_theta_l = self.current_theta_l
-    print("wr: ")
-    print(self.wr)
-    print("wl: ")
-    print(self.wl)
     self.pub_wr.publish(self.wr)
     self.pub_wl.publish(self.wl)
 
+  def cmd_vel_cb(self, msg):
+    self.v = msg.linear.x
+    self.w = msg.angular.z
+    [self.wr.data, self.wl.data] = self.get_wheel_speeds(self.v, self.w)
 
-
+  def get_wheel_speeds(self, v, w):
+    #Get the wheel speeds from robot velocity
+    wr = (2*v + w * self.L)/(2*self.r)
+    wl = (2*v - w * self.L)/(2*self.r)
+    return [wr, wl]
 
 
 
