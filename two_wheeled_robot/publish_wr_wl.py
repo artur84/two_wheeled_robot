8d947c052f4fab98b08a1f7b5ebca33d4a79b9e5
Arturo Escobedo <arturoescobedo.iq@gmail.com>
2024-04-20 07:22:03 -0600

    Changed to publish wr and wl from wheel odometry published by the simulator, efficient and accurate with the simulator

--- two_wheeled_robot/publish_wr_wl.py
+++ two_wheeled_robot/publish_wr_wl.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 import rclpy
 from rclpy.node import Node
 
@@ -10,7 +11,7 @@ class PublishWheelSpeeds(Node):
 
   def __init__(self):
     super().__init__('publish_wr_wl')
-    self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_cb, 10 )
+    self.create_subscription(Odometry, 'wheel/odometry', self.odom_cb, 10 )
     # Create wr publisher
     self.pub_wr = self.create_publisher(Float32, 'VelocityEncR', 10)
     # Create wr publisher
@@ -21,22 +22,19 @@ class PublishWheelSpeeds(Node):
     self.r = 0.14 # radius of the wheels in [m]
     self.wr = Float32()
     self.wl = Float32()
-    self.v = 0.0
-    self.w = 0.0
-    self.robot_vel = Twist()
 
     # Call on_timer function every second
-    self.dt= 0.02
+    self.dt= 0.04
     self.timer = self.create_timer(self.dt, self.on_timer)
 
   def on_timer(self):
     self.pub_wr.publish(self.wr)
     self.pub_wl.publish(self.wl)
 
-  def cmd_vel_cb(self, msg):
-    self.v = msg.linear.x
-    self.w = msg.angular.z
-    [self.wr.data, self.wl.data] = self.get_wheel_speeds(self.v, self.w)
+  def odom_cb(self, msg):
+    v = msg.twist.twist.linear.x
+    w = msg.twist.twist.angular.z
+    [self.wr.data, self.wl.data] = self.get_wheel_speeds(v, w)
 
   def get_wheel_speeds(self, v, w):
     #Get the wheel speeds from robot velocity
